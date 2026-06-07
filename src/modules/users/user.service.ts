ccb068dd1d27fd8f5d57182349ab80fea2649033
sumai-suchi <sumaiyamoina@gmail.com>
2026-06-08 01:26:39 +0600

    hashed the password with bcrypt

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,10 +1,11 @@
 import { pool } from "../../db/database"
 import type { IUser } from "./user.interface"
-
+import bcrypt from 'bcrypt'
 const createUserIntoDB= async (payload : IUser)=>{
     const {name,email,password,role}= payload
+    const hashedPassword = await bcrypt.hash(password , 10)
     const result = await pool.query(`
-        INSERT INTO users (name, email, password,role) VALUES ($1, $2, $3, $4) RETURNING *`,[name,email,password,role || 'contributor'])
+        INSERT INTO users (name, email, password,role) VALUES ($1, $2, $3, $4) RETURNING *`,[name,email,hashedPassword,role || 'contributor'])
       console.log(result)
       return result
 }
